713e57ffbefcf8fd38b3ef09c377e9b07d1dd36d
ashleyzawadi <zawadikibathi@gmail.com>
2018-06-26 20:22:59 +0300

    Set up project to serve up static files

--- arcade/urls.py
+++ arcade/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('shots.urls')),
 ]
+
 
 
