e01bf3982b92b31e65e2c4ea67fea83cde599ff2
ashleyzawadi <zawadikibathi@gmail.com>
2018-06-26 19:32:56 +0300

    Create superuser, register models and test admin dashboard

--- shots/admin.py
+++ shots/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Location,Category,Image
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
